d6082bca2f462a7446900fda36e2ca410e6a57ee
Jason Fowler <jason_fowler@acl.com>
2014-03-10 10:50:06 -0700

    fixed month issue

--- ss1.go
+++ ss1.go
@@ -16,6 +16,7 @@ import (
 	"net/http"
 	"time"
 	"strings"
+      "strconv"
 )
 
 
@@ -44,63 +45,69 @@ func check(e error) {
 } //check
 
 func getStocks(symbol string) {
-    
-    t := time.Now().Format("2006-01-02")
+
+      t := time.Now().Format("2006-01-02")
 	tArray := strings.Split(t, "-")
 
  	nowyear := tArray[0]
- 	nowmonth := tArray[1]
+ 	nmonth := tArray[1]
+      nmonth2, err := strconv.Atoi(nmonth)
+      check(err)
+      nowmonth := (nmonth2 - 1)
  	nowday := tArray[2]
 
-  	thenyear := 2014
-  	thenmonth := 00
-  	thenday := 01
+  	p := time.Now().AddDate(0, 0, -60).Format("2006-01-02")
+      pArray := strings.Split(p, "-")
+      thenyear := pArray[0]
+      tmonth := pArray[1]
+      tmonth2, err := strconv.Atoi(tmonth)
+      check(err)
+      thenmonth := (tmonth2 - 1)
+      thenday := pArray[2]
+      //fmt.Println(thenyear, thenmonth, thenday, " - ", nowyear,nowmonth,nowday)
 
-	url := fmt.Sprintf("http://ichart.finance.yahoo.com/table.csv?s=%s&a=%d&b=%d&c=%d&d=%s&e=%s&f=%s&g=d", symbol, thenmonth, thenday, thenyear, nowmonth, nowday, nowyear)
+	url := fmt.Sprintf("http://ichart.finance.yahoo.com/table.csv?s=%s&a=%d&b=%s&c=%s&d=%d&e=%s&f=%s&g=d", symbol, thenmonth, thenday, thenyear, nowmonth, nowday, nowyear)
 	resp, err := http.Get(url)
-	if resp.StatusCode != 200 {
-		fmt.Println(symbol+": error with http response code")
+	if resp.StatusCode > 399 {
+		fmt.Println(symbol+": not on yahoo finance")
 		return
 	}
+      defer resp.Body.Close()
 
 	os.Remove(symbol+".db")
-    db, err := sql.Open("sqlite3", symbol+".db")
+      db, err := sql.Open("sqlite3", symbol+".db")
 	if err != nil {
 		fmt.Println("error opening db")
 	}
 	defer db.Close()
 	_, err = db.Exec("CREATE TABLE stockhistory (id INTEGER NOT NULL PRIMARY KEY, ydate TEXT, closeprice INTEGER);")
-    if err != nil {
+      if err != nil {
         fmt.Println("could not create table")
-    }
-    _, err = db.Exec("CREATE TABLE slopedata (id INTEGER NOT NULL PRIMARY KEY, slope FLOAT64, tradingdays INTEGER);")
-    if err != nil {
-        fmt.Println("could not create table")
-    }
+      }
+     _, err = db.Exec("CREATE TABLE slopedata (id INTEGER NOT NULL PRIMARY KEY, slope FLOAT64, tradingdays INTEGER);")
+      if err != nil {
+          fmt.Println("could not create table")
+      }
 
-	defer resp.Body.Close()
-	
-	csvReader := csv.NewReader(resp.Body)
+      csvReader := csv.NewReader(resp.Body)
 	records, err := csvReader.ReadAll()
-	if err != nil {
-          fmt.Println(symbol+" error reading csv")
-	}
+	check(err)
 	records = append(records[:0], records[0+1:]...)
 		for _, record := range records {
 			d := record[0]
 		   	c := record[4]
-			
+
 			tx, err := db.Begin()
 				if err != nil {
 					fmt.Println("error with db")
 				}
 			insert_stmt, err := tx.Prepare("insert into stockhistory(ydate,closeprice) values(?,?)")
-				
+
 			defer insert_stmt.Close()
 			_, err = insert_stmt.Exec(d,c)
 			tx.Commit()
 		}
-		
+
 	var gslf GetSlopesFunc
 	gslf = getSlope
 	gslf(symbol)
@@ -110,12 +117,9 @@ func getStocks(symbol string) {
 func getSlope(symbol string) {
 	//get slope
 	ntd := 35.00
-	fname := "Slopes.csv"
 
 	db, err := sql.Open("sqlite3", symbol+".db")
-	if err != nil {
-		fmt.Println("error opening db")
-	}
+	check(err)
 	defer db.Close()
 	rows, err := db.Query("select sum(id) as sumx, sum(closeprice) as sumy, sum(id * closeprice) as sumxy, sum(id * id) as sumxx from(select id, closeprice from stockhistory order by ydate desc limit ?);", ntd)
 		if err != nil {
@@ -136,16 +140,18 @@ func getSlope(symbol string) {
 			sumxsumx := sumx * sumx
 
 			slope := (ntdsumxy - sumxsumy) / (ntdsumxx - sumxsumx)
-            fmt.Println(symbol,slope)            
+            fmt.Println(symbol,slope)
 		}
 		rows.Close()
-    	f, err := os.OpenFile(fname, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0666)
-    	check(err)
-    	var textout string
-    	fmt.Scanln(&textout)
-    	_, err = f.Write([]byte(textout))
-    	check(err)
-    	f.Close()
+
+     //  fname := "Slopes.csv"
+    	// f, err := os.OpenFile(fname, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0666)
+    	// check(err)
+    	// var textout string
+    	// fmt.Scanln(&textout)
+    	// _, err = f.Write([]byte(textout))
+    	// check(err)
+    	// f.Close()
 
 		// fmt.Fprint(w, symbol+",")
 		// fmt.Fprint(w, slope)
@@ -154,8 +160,8 @@ func getSlope(symbol string) {
 } //getSlope
 
 func main(){
-	symbols, err := readLines("testsymbols.txt") 
-    //symbols, err := readLines("stocks-testing.txt") 
+	symbols, err := readLines("testsymbols.txt")
+    //symbols, err := readLines("stocks-testing.txt")
   	if err != nil {
     	fmt.Println("readLines error reading")
   	}
@@ -164,7 +170,7 @@ func main(){
 	 	gsf = getStocks
 	 	gsf(symbol)
 	}
-	
+
 } // func main
 
 
