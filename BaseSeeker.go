acf3bdeb256120ce09a55485a9541c38dd4cf0ea
Jason Fowler <preradio@gmail.com>
2014-03-15 15:13:15 -0700

    lt 120 days and 0 price rejection

--- BaseSeeker.go
+++ BaseSeeker.go
@@ -46,7 +46,7 @@ func Init(errorHandle io.Writer) {
 
 func check(e error) {
     if e != nil {
-      ERROR.Println(e)
+      log.Println(e)
     }
 } //check
 
@@ -62,7 +62,7 @@ func getStocks(symbol string) {
   nowmonth := (nmonth2 - 1)
  	nowday := tArray[2]
 
-	p := time.Now().AddDate(0, 0, -725).Format("2006-01-02")
+	p := time.Now().AddDate(0, 0, -729).Format("2006-01-02")
   pArray := strings.Split(p, "-")
   thenyear := pArray[0]
   tmonth := pArray[1]
@@ -78,30 +78,46 @@ func getStocks(symbol string) {
 		return
 	}
   defer resp.Body.Close()
-
-	db, err := sql.Open("sqlite3", "db/"+symbol+".db")
-	check(err)
-	defer db.Close()
-	_, err = db.Exec("CREATE TABLE stockhistory (id INTEGER NOT NULL PRIMARY KEY, ydate TEXT, closeprice INTEGER);")
-  check(err)
-
   csvReader := csv.NewReader(resp.Body)
 	records, err := csvReader.ReadAll()
 	check(err)
-	records = append(records[:0], records[0+1:]...)
+  records = append(records[:0], records[0+1:]...)
+
+   // if not >120 lines, skip
+  lineCount := 0
+  for _ = range records {
+    lineCount += 1
+  }
+  if lineCount < 121 {
+    return
+  }
+
+  // if no data, skip - check closing price
+  for _, record := range records {
+    c := record[4]
+    if c == "0.00" {
+      return
+    }
+  }
+
+  // okay, good to go
+  db, err := sql.Open("sqlite3", "db/"+symbol+".db")
+  check(err)
+  defer db.Close()
+  _, err = db.Exec("CREATE TABLE stockhistory (id INTEGER NOT NULL PRIMARY KEY, ydate TEXT, closeprice INTEGER);")
+  check(err)
+
 	for _, record := range records {
 		d := record[0]
-	   	c := record[4]
-
-		tx, err := db.Begin()
-			check(err)
-		insert_stmt, err := tx.Prepare("insert into stockhistory(ydate,closeprice) values(?,?)")
+	  c := record[4]
+  	tx, err := db.Begin()
+  	check(err)
+  	insert_stmt, err := tx.Prepare("insert into stockhistory(ydate,closeprice) values(?,?)")
     check(err)
-		defer insert_stmt.Close()
-		_, err = insert_stmt.Exec(d,c)
+  	defer insert_stmt.Close()
+  	_, err = insert_stmt.Exec(d,c)
 		tx.Commit()
-		}
-
+  }
 } // getStocks
 
 func getSlope(symbol string, ntd float64, slope float64, ch chan bool) {
@@ -152,6 +168,13 @@ func main() {
   os.Remove("Slopes.csv")
   os.RemoveAll("./db")
   os.Mkdir("./db", 0700)
+  f, err := os.OpenFile("baseseeker_log.txt", os.O_RDWR | os.O_CREATE | os.O_APPEND, 0666)
+  if err != nil {
+    fmt.Printf("error opening file: %v", err)
+  }
+defer f.Close()
+
+log.SetOutput(f)
 	symbols, err := readLines("symbols-medium.txt")
   check(err)
 	for _, symbol := range symbols {
